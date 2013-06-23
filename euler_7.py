e3c13fd2ad097ee8afe299cec54d31ed98fef661
cjwfries <cjwfries@gmail.com>
2013-06-22 23:52:37 -0700

    Euler #7 comments

--- euler_7.py
+++ euler_7.py
@@ -1,3 +1,16 @@
+"""
+06/22/13
+
+Project Euler #4 10001st prime
+    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+    What is the 10 001st prime number?
+    
+Algorithm
+    Not very elegant- it's based on guessing the upper bound of the 10001st prime, which I guessed to be 300000.
+    I thought of making this function more flexible and allow us to find primes past the 10001st prime by constantly guessing a bigger number based on the number of primes we are able to generate by the first guess, however it wouldn't be very practical, seeing how long it took the computer to find 10001 primes.
+    After this, I used the Sieve of Eratosthenes.
+"""
+
 def getPrimes(num):
     ret = []
     sieve = []
